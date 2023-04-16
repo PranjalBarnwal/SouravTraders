97be32db1046dac15f9cade229b114a66282c9c9
Pranjal <happybarnwal007@gmail.com>
2023-04-16 20:32:21 +0530

    updated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { RxCross1 } from "react-icons/rx";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const navHandler = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <>
+      <div className="navbar">
+        <div className="navbar-title">Sourav Traders</div>
+
+        {toggle ? (
+          <div className="navbar-close" onClick={navHandler}>
+            <RxCross1 />
+          </div>
+        ) : (
+          <div className="navbar-open" onClick={navHandler}>
+            <RxHamburgerMenu />
+          </div>
+        )}
+
+        <div className="navbar-list">
+          <a href="#">Home</a>
+          <a href="#">Products</a>
+          <a href="#">Reach Us</a>
+          <a href="#">About Us</a>
+        </div>
+      </div>
+
+      <div className="responsive-nav-parent">
+        <div className={toggle ? "responsive-nav nav-toggle" : "responsive-nav"}>
+          <a href="#">Home</a>
+          <a href="#">Products</a>
+          <a href="#">Reach Us</a>
+          <a href="#">About Us</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
